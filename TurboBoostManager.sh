ddb68f33192f512f53f2469e1bee82d509bde756
dchakro <35454738+dchakro@users.noreply.github.com>
2020-09-11 06:29:32 +0300

    Moved function definitions to a separate file

--- TurboBoostManager.sh
+++ TurboBoostManager.sh
@@ -1,47 +1,6 @@
 #!/usr/bin/env sh
 
-RED='\033[91m'
-GREEN='\033[92m'
-CYAN='\033[96m'
-NC='\033[0m'
-
-CHECK_STATUS()
-{
-    result=`kextstat | grep -c com.rugarciap.DisableTurboBoost`
-}
-
-PRINT_STATUS()
-{
-    echo
-    echo "Checking status of Turbo Boost..."
-    CHECK_STATUS
-    if [ $result -eq 0 ]; then
-        echo "[${RED}Kext is not loaded${NC}]" 
-        echo "TurboBoost is currently [${RED}enabled${NC}]"
-    else
-        echo "[${CYAN}Kext 'com.rugarciap.DisableTurboBoost' is loaded.${NC}]"
-        echo "Turbo Boost status: ${GREEN}disabled${NC}"
-    fi
-    echo
-}
-
-LOAD()
-{    
-    # Disables Turbo Boost
-    # loads the pre-signed kext from Turbo Boost Switcher
-    echo "[${RED}!${NC}]Disabling TurboBoost now...${NC}"
-    sudo /usr/bin/kextutil -q '/Applications/Turbo Boost Switcher.app/Contents/Resources/DisableTurboBoost.64bits.kext'
-    PRINT_STATUS
-}
-
-UNLOAD()
-{
-    # Enables Turbo Boost
-    # unloads the pre-signed kext from Turbo Boost Switcher
-    echo "[${RED}!${NC}]Unloading kext now...${NC}"
-    sudo /sbin/kextunload '/Applications/Turbo Boost Switcher.app/Contents/Resources/DisableTurboBoost.64bits.kext'
-    PRINT_STATUS    
-}
+source ./functions.sh
 
 while true
 do
@@ -63,13 +22,14 @@ do
                 break
             else
                 PRINT_STATUS
+                sleep 0.5
             fi
-            break
             ;;
         2) 
             CHECK_STATUS
             if [ $result -eq 0 ]; then
                 PRINT_STATUS
+                sleep 0.5
             else
                 UNLOAD
                 break
@@ -84,6 +44,7 @@ do
             ;;
         4)
             PRINT_STATUS
+            sleep 0.5
             ;;
         5)
             exit 0
@@ -97,3 +58,4 @@ do
             continue
     esac
 done
+sudo -k 
