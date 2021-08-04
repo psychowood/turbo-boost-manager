7f365dfe51730564a7005e895a3d3d575e95ea22
psychowood <psychowood@gmail.com>
2021-08-04 22:38:23 +0200

    Add cli options + reformat menu

--- TurboBoostManager.sh
+++ TurboBoostManager.sh
@@ -32,15 +32,25 @@ before running Turbo Boost Manager again, otherwise you'll get permission-relate
 
 	PRINT_STATUS
 
-	printf '
-	1) Disable Turbo Boost
-	2) Enable Turbo Boost
-	3) Re-Disable
-	4) Check status
-	5) Exit
+	printf '   
+   1) Disable Turbo Boost
+   2) Enable Turbo Boost
+   3) Re-Disable
+   4) Check status
+   5) Exit
+
+You can also pass the number on the command line to run it automatically, as in
+'$0' 1
+to Disable Turbo Boost on start.
 	  
 	Enter: ';
-	read var;
+   Enter: ';
+    if [ $1 ]; then
+        var=$1
+    else 
+	    read var;
+    fi
+
 	# option #3 Re-disable unloads the kext and loads it again
 	case $var in
 	    1)  
@@ -81,7 +91,11 @@ before running Turbo Boost Manager again, otherwise you'll get permission-relate
             echo "Try again..."
             echo
             sleep 0.5
-            continue
+            if [ $1 ]; then
+                exit 0
+            else
+                continue
+            fi
     esac
     if [ $1 ]; then
         exit 0
